023bd51be125143bd3380854a798d6e7991ab57e
Tudor Raul Ciprian <raulciprian.tudor@gmail.com>
2026-07-09 09:55:14 +0200

    chore: sync package-lock.json / .angular / update eslint to ignore .angular / .angular/..

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,7 @@ export default [
   ...nx.configs['flat/typescript'],
   ...nx.configs['flat/javascript'],
   {
-    ignores: ['**/dist', '**/out-tsc', '**/.angular/**'],
+    ignores: ['**/dist', '**/out-tsc', '**/angular', '**/.angular/**'],
   },
   {
     files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
